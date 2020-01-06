e30d597ff53784e7193bf7db5cc2214f10c518f1
davidy9000 <davidy.9000@gmail.com>
2020-01-06 13:08:37 -0500

    inital commits and first 3 questions

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function buttonImRight1(){
+    document.getElementById("button2").innerHTML = "No, I'm Right";
+}
+
+function buttonImRight2(){
+    document.getElementById("button1").innerHTML = "No, I'm Right";
+}
+
+function dontHover(){
+    // document.getElementById("donthover").innerHTML = "Hey, I told you not to hover over me!";
+    alert("Hey, I told you not to hover over me!");
+}
